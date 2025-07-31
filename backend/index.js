259bddc11b31d543193217a9fc4210d2d86c2598
Paritosh <barmanpari163@gmail.com>
2025-07-31 12:52:37 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST'],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
